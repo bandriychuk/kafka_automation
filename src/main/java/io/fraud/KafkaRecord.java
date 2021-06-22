96d64264aa64aa9ba9b3f0e061ccc6a6df40ff13
borysAndriychuk <borys.andriychuk@otakoyi.com.ua>
2021-06-22 15:21:12 +0300

    Added constructor for KafkaRecord,

--- src/main/java/io/fraud/KafkaRecord.java
+++ src/main/java/io/fraud/KafkaRecord.java
@@ -4,4 +4,12 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class KafkaRecord {
 
+    private final ConsumerRecord<String, String> record;
+
+    public KafkaRecord(ConsumerRecord<String, String> record) {
+        this.record = record;
+    }
+
+
+
 }

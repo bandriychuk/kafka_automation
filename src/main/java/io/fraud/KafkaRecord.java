478e1533ad15052002676076db76cddbe672e76d
borysAndriychuk <borys.andriychuk@otakoyi.com.ua>
2021-06-22 16:23:02 +0300

    Added valueAs method

--- src/main/java/io/fraud/KafkaRecord.java
+++ src/main/java/io/fraud/KafkaRecord.java
@@ -1,16 +1,25 @@
 package io.fraud;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class KafkaRecord {
 
     private final ConsumerRecord<String, String> record;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
 
     public KafkaRecord(ConsumerRecord<String, String> record) {
         this.record = record;
     }
 
-    public boolean hasSourceId(String message){
+    public boolean hasSourceId(String message) {
         return record.value().contains(message);
+    }
+
+    @SneakyThrows
+    public <T> T valueAs(Class<T> tClass) {
+        return objectMapper.readValue(record.value(), tClass);
     }
 }

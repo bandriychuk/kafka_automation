b033c5da385a9806b951fa2e4c94c9ab93818a71
borysAndriychuk <borys.andriychuk@otakoyi.com.ua>
2021-06-22 15:25:14 +0300

    Added constructor for KafkaProducer

--- src/main/java/io/fraud/producer/KafkaMessageProducer.java
+++ src/main/java/io/fraud/producer/KafkaMessageProducer.java
@@ -1,4 +1,14 @@
 package io.fraud.producer;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+
 public class KafkaMessageProducer {
+
+    private static KafkaProducer<String, String> kafkaProducer;
+    private final String bootStrapServer;
+
+    public KafkaMessageProducer(String bootStrapServer) {
+        this.bootStrapServer = bootStrapServer;
+    }
+
 }

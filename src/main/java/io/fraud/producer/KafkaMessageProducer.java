4088f759d7a23c9f8a1e846d1302a0fe6bebedc4
borysAndriychuk <borys.andriychuk@otakoyi.com.ua>
2021-06-22 15:26:07 +0300

    Added methods createProducer, createProducerProperties, send

--- src/main/java/io/fraud/producer/KafkaMessageProducer.java
+++ src/main/java/io/fraud/producer/KafkaMessageProducer.java
@@ -1,6 +1,13 @@
 package io.fraud.producer;
 
+import lombok.SneakyThrows;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 public class KafkaMessageProducer {
 
@@ -11,4 +18,30 @@ public class KafkaMessageProducer {
         this.bootStrapServer = bootStrapServer;
     }
 
+    public KafkaMessageProducer createProducer() {
+        Properties properties = createProducerProperties();
+        if (kafkaProducer == null) {
+            kafkaProducer = new KafkaProducer<>(properties);
+        }
+        return this;
+    }
+
+    private Properties createProducerProperties() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootStrapServer);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "TkKafkaProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    @SneakyThrows
+    public RecordMetadata send(String topic, String message) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
+//        try {
+        return kafkaProducer.send(record).get();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+    }
 }

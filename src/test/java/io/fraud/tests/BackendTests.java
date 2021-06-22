cd147925110f8bc2a9e914fd686c11e1763b8f54
borysAndriychuk <borys.andriychuk@otakoyi.com.ua>
2021-06-22 16:23:42 +0300

    Added testApplicationCanProcessValidMessage

--- src/test/java/io/fraud/tests/BackendTests.java
+++ src/test/java/io/fraud/tests/BackendTests.java
@@ -1,27 +1,50 @@
 package io.fraud.tests;
 
 import io.fraud.KafkaRecord;
+import io.fraud.KafkaService;
 import io.fraud.consumer.KafkaMessageConsumer;
+import io.fraud.messages.DealMessage;
+import io.fraud.messages.GeneratorMessage;
 import io.fraud.producer.KafkaMessageProducer;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.assertj.core.internal.bytebuddy.utility.RandomString;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BackendTests {
 
+    private final KafkaService kafkaService = new KafkaService("localhost:9092");
 
     @Test
     public void testCanWriteMessageToQueuingTransaction() {
-        KafkaMessageProducer kafkaMessageProducer = new KafkaMessageProducer("localhost:9092");
-        KafkaMessageConsumer messageConsumer = new KafkaMessageConsumer("localhost:9092");
-        messageConsumer.subscribe("test");
-        messageConsumer.consume();
+        kafkaService.subscribe("test");
+        kafkaService.send("test","Hello from Java 8");
 
-        kafkaMessageProducer.send("test","Hello from Java 8");
-
-        KafkaRecord receivedRecords = messageConsumer.waitForMessage("Hello from Java 8");
+        KafkaRecord receivedRecords = kafkaService.waitForMessage("Hello from Java 8");
 
         assertThat(receivedRecords).isNotNull();
         assertThat(receivedRecords).isNotNull();
     }
+
+    @Test
+    public void testApplicationCanProcessValidMessage() {
+        GeneratorMessage generatorMessage = new GeneratorMessage();
+        generatorMessage.setData(new Date().toString());
+        generatorMessage.setAmount(2000);
+        generatorMessage.setCurrency("EUR");
+        generatorMessage.setSource(RandomStringUtils.randomAlphabetic(10));
+        generatorMessage.setSource(RandomStringUtils.randomAlphabetic(10));
+
+        kafkaService.subscribe("streaming.transactions.legit");
+        kafkaService.send("queuing.transactions", generatorMessage);
+
+        DealMessage dealMessage = kafkaService.waitForMessage("java12").valueAs(DealMessage.class);
+
+        assertThat(dealMessage.getAmount()).isEqualTo(900.0);
+        assertThat(dealMessage.getBaseCurrency()).isEqualTo("USD");
+    }
+
 }
